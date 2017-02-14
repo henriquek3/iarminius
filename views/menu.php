a2be19518cb78bf8d44cc028f6573d7dfbfa12e6
Jean Freitas <henriquek3@live.com>
2017-02-14 15:09:07 -0300

    Initial

--- views/menu.php
+++ views/menu.php
@@ -1,42 +1,42 @@
 <nav class="menu-n navbar navbar-light bg-faded">
     <div class="container">
-        <!-- Brand -->
-        <a class="navbar-brand" href="../index.php"><img src="../views/home.jpg" border="0"></a>
-
-        <!-- Links -->
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="cor nav-link" href="../index.php">Home</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                   aria-expanded="false">
-                    Cadastrar
-                </a>
-                <div class="dropdown-menu" aria-labelledby="Preview">
-                    <a class="cor dropdown-item" href="../views/cadastro-form-sinodo.php">Sínodo</a>
-                    <a class="cor dropdown-item" href="../views/cadastro-form-presbiterio.php">Presbitério</a>
-                    <a class="cor dropdown-item" href="../views/cadastro-form-igreja.php">Igreja</a>
-                    <a class="cor dropdown-item" href="../views/cadastro-form-ministro.php">Oficiais</a>
-                </div>
-
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                   aria-expanded="false">
-                    Relatórios
-                </a>
-                <div class="dropdown-menu" aria-labelledby="Preview">
-                    <a class="dropdown-item" href="../views/cadastro-form-relatorio.php">Estatísticas de Comunidade</a>
-                </div>
-
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Sair</a>
-            </li>
-        </ul>
-
+        <div>
+            <!-- Brand -->
+            <a class="navbar-brand" href="../index.php"><img src="../views/home.jpg" border="0"></a>
+            <!-- Links -->
+            <ul class="nav navbar-nav" style="padding-top: 8px">
+                <li class="nav-item">
+                    <a class="cor nav-link" href="../index.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        Cadastros
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="Preview">
+                        <a class="cor dropdown-item" href="../views/cadastro-form-sinodo.php">Sínodos</a>
+                        <a class="cor dropdown-item" href="../views/cadastro-form-presbiterio.php">Presbitérios</a>
+                        <a class="cor dropdown-item" href="../views/cadastro-form-igreja.php">Igrejas</a>
+                        <a class="cor dropdown-item" href="../views/cadastro-form-ministro.php">Oficiais</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        Relatórios
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="Preview">
+                        <a class="dropdown-item" href="../views/cadastro-form-relatorio.php">Estatísticas de
+                            Comunidade</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sair</a>
+                </li>
+            </ul>
+        </div>
     </div>
 
 </nav>
